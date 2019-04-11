33df049d9408f4242b5fb82e383a64e6043af271
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-04-11 14:12:13 -0500

    Create string.cpp

---
+++ Clase_string/string.cpp
@@ -0,0 +1,29 @@
+#include "my_string.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+my_string::my_string (const char * text){
+	tamano(text);
+	A = new char[tam];
+	for(int i=0;i<tam;i++)
+		A[i]=text[i];}
+my_string::~my_string(){
+	delete[] A;}
+my_string::tamano(const char * text){
+	while(text != '\0')tam++;
+	tam+=1;
+	return tam;
+}
+my_string::my_string(const my_string & text){
+	int b=text.tam;
+	delete[] A;
+	A=new char[b];
+	for(int i=0;i<b;i++)
+		A[i]=text.A[i];}
+my_string::my_string & operator = (const my_string & text){
+	delete[]A;
+	int b=text.tam;
+	A=new char[b];
+	for(int i=0;i<b;i++)
+		A[i]=text.A[i];}
+int main(int argc, char** argv) {
+	return 0;
+}
