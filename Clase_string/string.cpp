ba0d4c7af549fb7c5f05977b5d1bef62a545bbfa
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-04-12 01:32:41 -0500

    Update string.cpp

--- Clase_string/string.cpp
+++ Clase_string/string.cpp
@@ -1,39 +1,90 @@
 #include "my_string.h"
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include<iostream>
 my_string::my_string (const char * text){
 	tamano(text);
 	A = new char[tam];
-	for(int i=0;i<tam;i++)
-		A[i]=text[i];}
+	for(int i=0;i<tam;i++){
+		A[i]=text[i];}}
 my_string::~my_string(){
 	delete[] A;}
 my_string::tamano(const char * text){
-	while(text != '\0')tam++;
-	tam+=1;
+    int i=0;
+    tam=0;
+	while(text[i] != '\0'){
+            i++;
+            tam++;}
+	tam++;
 	return tam;
 }
 my_string::my_string(const my_string & text){
 	int b=text.tam;
 	delete[] A;
 	A=new char[b];
+	tam=b;
 	for(int i=0;i<b;i++)
 		A[i]=text.A[i];}
 my_string& my_string::operator = (const my_string & text){
 	int b=text.tam;
 	delete[] A;
 	A=new char[b];
+	tam=b;
 	for(int i=0;i<b;i++)
 		A[i]=text.A[i];
+}
+my_string& my_string::operator = (const char* text){
+	int b=tamano(text);
+	delete[] A;
+	A=new char[b];
+	tam=b;
+	for(int i=0;i<b;i++)
+		A[i]=text[i];
 }
 my_string& my_string::operator + (const my_string & text){
 	int b=text.tam;
 	int c=0;
-	A=new char[b];
-	for(int i=0;i<b;i++)
-		
-		A[tam-1+i]=text.A[i];
+	char *aux=new char[tam];
+	for(int i=0;i<tam;i++)aux[i]=A[i];
+	A=new char[b-1];
+	for(int i=0;i<tam;i++) A[i]=aux[i];
+    for(int i=tam-1;i<tam+b-1;i++) A[i]=text.A[c++];
+    tam=tam+b-1;
+    delete[] aux;
+}
+my_string& my_string:: operator +(const  char* text){
+    int i=0;
+    int b=0;
+	while(text[i] != '\0'){
+            i++;
+            b++;}
+	b++;
+	int c=0;
+	char *aux=new char[tam];
+	for(i=0;i<tam;i++)aux[i]=A[i];
+	A=new char[b-1];
+	for(i=0;i<tam;i++) A[i]=aux[i];
+    for(i=tam-1;i<tam+b-1;i++) A[i]=text[c++];
+    tam=tam+b-1;
+    delete[] aux;
+}
+bool my_string:: operator == (const my_string & text){
+    if(tam!=text.tam) return false;
+    for(int i=0;i<tam;i++)
+        if(A[i]!=text.A[i])  return false;
+    return true;}
+ostream & my_string::  operator << (ostream & os ,const my_string &texto){
+
+}
+int main() {
+	my_string prim("h");
+	my_string prim2("hola");
+	my_string prim3("bye");
+	if(prim==prim2)cout<<"verdadero"<<endl;
+	prim3+prim;
+	prim2+"adios";
+	prim2+"no";
+
+	prim.print();
+	prim2.print();
+	prim3.print();
 }
 
-int main(int argc, char** argv) {
-	return 0;
-}
