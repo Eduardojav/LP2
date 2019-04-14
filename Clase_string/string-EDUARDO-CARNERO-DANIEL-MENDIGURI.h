28d538c01c8671a84288b624aafb3fadb474858d
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-04-14 00:14:09 -0500

    Update string-EDUARDO-CARNERO-DANIEL-MENDIGURI.h

--- Clase_string/string-EDUARDO-CARNERO-DANIEL-MENDIGURI.h
+++ Clase_string/string-EDUARDO-CARNERO-DANIEL-MENDIGURI.h
@@ -2,37 +2,37 @@
 using namespace std;
 class my_string{
 	private:
-		char *A;
+		char *A=NULL;
 		int tamano(const char * text=NULL);
 		int tamo(char * text=NULL);
-	public:
-	    int tam;
-	    void print(){
-	    for(int i=0;i<tam;i++)
-		cout<<A[i];
-		cout<<endl;}
+		int tam;
+		public:
 		my_string();
-		my_string(const char * text=NULL);
+		my_string(const char * text);
 		my_string(const my_string & text);
 		~my_string();
 		my_string & operator = (const my_string & text);
 		my_string & operator = (const char* text);
 		my_string & operator + (const my_string & text);
 		my_string & operator + (const char* text);
+		my_string & operator += (const my_string & text);
+		my_string & operator += (const char* text);
 		my_string &operator[](const my_string & text);
 		bool  operator == (const my_string & text);
+		bool  operator < (const my_string & text);
+		bool  operator > (const my_string & text);
+		bool  operator <= (const my_string & text);
+		bool  operator >= (const my_string & text);
 		char & operator[](int  i);
 		friend std::istream &operator >>(std::istream & is, my_string &str){
-        char* c = new char[1000];
-        is >> c;
-        str = my_string(c);
-        delete[] c;
-        return is;
+            char* c = new char[1000];
+            is >> c;
+            str = my_string(c);
+            delete[] c;
+            return is;
         }
         friend std::ostream &operator <<(std::ostream & os,const my_string &str){
-        int b=str.tam;
-        for(int i=0;i<b;i++)
-            os<<str.A[i];
-        return os;
+            return os<<str.A;
     }
 };
+
