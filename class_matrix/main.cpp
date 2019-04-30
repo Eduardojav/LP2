be445145e116409f26b16fecd847d555d5f0ec3c
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-04-30 11:36:31 -0500

    Update main.cpp

--- class_matrix/main.cpp
+++ class_matrix/main.cpp
@@ -6,11 +6,12 @@ int main()
 {
     Matrix<int> uno(3,3);
     Matrix<int> dos(3,3);
-    uno<<1<<2<<3<<4<<5<<6<<7<<8<<9<<10;
+    uno<<1<<2<<3<<4<<5<<6<<7<<8<<9<<10<<12<<14;
     dos<<2<<11<<12<<13<<14<<15<<16<<17<<18;
     cout<<uno<<endl;
     cout<<dos<<endl;
     cout<<dos*uno<<endl;
+    cout<<uno (1,1)<<endl;
     cout<<uno+dos<<endl;
     return 0;
 }
