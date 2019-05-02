3d0c23ece1c57f888149011917c0451b26c50626
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-02 12:50:57 -0500

    Update main.cpp

--- class_matrix/main.cpp
+++ class_matrix/main.cpp
@@ -1,17 +1,36 @@
-
-#include<iostream>
+#include <iostream>
 #include "declaration.h"
+
 using namespace std;
+
 int main()
 {
     Matrix<int> uno(3,3);
     Matrix<int> dos(3,3);
-    uno<<1<<2<<3<<4<<5<<6<<7<<8<<9<<10<<12<<14;
-    dos<<2<<11<<12<<13<<14<<15<<16<<17<<18;
+    Matrix<int> cuatro(3,3);
+    uno.llenar();
+    uno<<1<<1<<1<<1<<1<<1<<1<<1<<1<<1<<1;
+    dos<<1<<1<<1<<1<<1<<1<<1<<1<<1<<1<<1;
+    Matrix<int> tres(uno);
+    cuatro=dos;
+    cuatro(1,1)=2;
+    cout<<"1.-esta es la uno"<<endl;
     cout<<uno<<endl;
+    cout<<"2.-esta es la dos"<<endl;
     cout<<dos<<endl;
-    cout<<dos*uno<<endl;
-    cout<<uno (1,1)<<endl;
-    cout<<uno+dos<<endl;
+    cout<<"3.-esta es la tres"<<endl;
+    cout<<tres<<endl;
+    cout<<"4.-esta es la cuatro"<<endl;
+    cout<<cuatro<<endl;
+    cout<<"5.-esta es la multiplicacion de la uno y dos"<<endl;
+    cout<<uno*dos<<endl;
+    cout<<"6.-esta es la suma de la dos y uno"<<endl;
+    cout<<uno-dos<<endl;
+    cout<<"7.-multiplicacion o division de la cuatro por una constante"<<endl;
+    cout<< cuatro/2<<endl;
     return 0;
 }
+
+
+
+
