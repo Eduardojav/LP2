a01009b5efde4888a918fad076721877c576b3d0
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-08 10:38:52 -0500

    Update string-funciones.cpp

--- Clase_string/string-funciones.cpp
+++ Clase_string/string-funciones.cpp
@@ -3,101 +3,101 @@
 #include "testingstr.h"
 #include<iostream>
 my_string::my_string(){
-            A=new char[0];}
+        A=new char[0];}
 my_string::my_string (const char * text){
-	tamano(text);
+	size(text);
 	A = new char[tam];
-	for(int i=0;i<tam;i++){
-		A[i]=text[i];}}
+	for(int i=0;i<tam;i++)
+		A[i] = text[i];}
 my_string::~my_string(){
 	delete[] A;}
 my_string::tamano(const char * text){
-    int i=0;
-    tam=0;
+    	int i = 0;
+    	siz = 0;
 	while(text[i] != '\0'){
             i++;
-            tam++;}
-	tam++;
-	return tam;
+            siz++;}
+	siz++;
+	return siz;
 }
 my_string::my_string(const my_string & text){
-	int b=text.tam;
+	int b = text.siz;
 	delete[] A;
-	A=new char[b];
-	tam=b;
-	for(int i=0;i<b;i++)
-		A[i]=text.A[i];}
+	A = new char[b];
+	siz = b;
+	for(int i = 0;i < b; i++)
+		A[i] = text.A[i];}
 my_string& my_string::operator = (const my_string & text){
-	int b=text.tam;
+	int b = text.siz;
 	delete[] A;
-	A=new char[b];
-	tam=b;
+	A = new char[b];
+	size = b;
 	for(int i=0;i<b;i++)
 		A[i]=text.A[i];
      return *this;
 }
 my_string & my_string::operator = (const char* text){
-	int b=tamano(text);
+	int b = tamano(text);
 	delete[] A;
-	A=new char[b];
-	tam=b;
-	for(int i=0;i<b;i++)
-		A[i]=text[i];
+	A = new char[b];
+	siz = b;
+	for(int i = 0; i < b ;i++)
+		A[i] = text[i];
     return *this;
 }
 my_string my_string :: operator + (const my_string text){
-	int b=text.tam;
-	int i=0;
-	int c=0;
+	int b = text.siz;
+	int i = 0;
+	int c = 0;
 	my_string aux;
-	aux.A=new char[b+tam];
-	aux.tam=tam+b;
-	for( i=0;i<tam;i++)aux[i]=A[i];
-    for( i=tam-1;i<tam+b;i++) aux[i]=text.A[c++];
+	aux.A = new char[b+siz];
+	aux.siz = siz+b;
+	for( i = 0 ; i < siz ; i++) aux[i] = A[i];
+    for( i=siz-1;i<siz+b;i++) aux[i]=text.A[c++];
     return aux;
 }
 my_string my_string :: operator +(const  char* text){
-    int i=0;
-    int b=0;
+    	int i=0;
+    	int b=0;
 	while(text[i] != '\0'){
             i++;
             b++;}
 	int c=0;
 	my_string aux;
-	aux.A=new char[b+tam];
-	aux.tam=tam+b;
-	for( i=0;i<tam;i++)aux[i]=A[i];
-    for( i=tam-1;i<tam+b;i++) aux[i]=text[c++];
+	aux.A=new char[b+siz];
+	aux.siz = siz + b;
+	for( i=0;i<siz;i++)aux[i]=A[i];
+    for( i = siz - 1 ; i < siz + b ; i++) aux[i] = text[c++];
     return aux;
 }
 char &my_string:: operator[]( int i){
-    if(i>=0&&i<tam)
+    if(i >= 0 && i < tam)
         return A[i];
     else{
         cout << "Posición incorrecta: " << i << endl;
     }}
 bool my_string:: operator == (const my_string & text){
-    if(tam!=text.tam) return false;
-    for(int i=0;i<tam;i++)
-        if(A[i]!=text.A[i])  return false;
+    if(siz != text.siz) return false;
+    for(int i = 0; i < siz ; i++)
+        if(A[i] != text.A[i])  return false;
     return true;}
 bool my_string:: operator < (const my_string & text){
     int t;
-    if(text.tam>tam){
-        t=tam;
+    if(text.siz>siz){
+        t = siz;
     }
     else{
-        t=text.tam;
+        t = text.siz;
     }
-    for(int i=0; i<t ;i++){
-        if(A[i]<text.A[i]){
+    for(int i  = 0; i < t ; i++){
+        if(A[i] < text.A[i]){
             return true;
         }
-        else if(A[i]>text.A[i]){
+        else if(A[i] > text.A[i]){
             return false;
         }
     }
-    if(text.tam>tam){
+    if(text.siz > siz){
         return true;
     }
     else{
@@ -106,97 +106,98 @@ bool my_string:: operator < (const my_string & text){
 }
 bool my_string:: operator > (const my_string & text){
     int t;
-    if(text.tam>tam){
-        t=tam;
+    if(text.siz > siz){
+        t = siz;
     }
     else{
-        t=text.tam;
+        t = text.siz;
     }
-    for(int i=0; i<t ;i++){
-        if(A[i]>text.A[i]){
+    for(int i = 0; i < t ;i++){
+        if(A[i] > text.A[i]){
             return true;
         }
-        else if(A[i]<text.A[i]){
+        else if(A[i] < text.A[i])
             return false;
-        }
     }
-    if(text.tam<tam){
+    if(text.siz < siz)
         return true;
-    }
-    else{
+	    
+    else
         return false;
-    }
 }
 bool my_string:: operator <= (const my_string & text){
     int t;
-    if(text.tam>tam){
-        t=tam;
+    if(text.siz>siz){
+        t = siz;
     }
     else{
-        t=text.tam;
+        t = text.siz;
     }
-    for(int i=0; i<t ;i++){
-        if(A[i]<text.A[i]){
+    for(int i = 0; i < t ; i++){
+        if(A[i] < text.A[i])
             return true;
-        }
-        else if(A[i]>text.A[i]){
+		
+        else if(A[i]>text.A[i])
             return false;
-        }
+        
     }
-    if(text.tam>tam){
+    if(text.siz>siz)
+	    
         return true;
-    }
-    else if(text.tam<tam){
+    
+    else if(text.siz < siz)
+	    
         return false;
-    }
-    else{
-        return true;
-    }
+    
+    else
+        
+	return true;
+  
 }
 bool my_string:: operator >= (const my_string & text){
     int t;
-    if(text.tam>tam)
-        t=tam;
+    if(text.siz > siz)
+	t = siz;
     else
-        t=text.tam;
-    for(int i=0; i<t ;i++){
-        if(A[i]>text.A[i])
-            return true;
-        else if(A[i]<text.A[i])
-            return false;}
-    if(text.tam<tam)
-        return true;
-    else if(text.tam>tam)
-        return false;
+	t = text.siz;
+    for(int i = 0; i < t ;i++){
+	if(A[i] > text.A[i])
+	    return true;
+	else if(A[i] < text.A[i])
+	    return false;}
+    if(text.siz < siz)
+	return true;
+    else if(text.siz > siz)
+	return false;
     else
-        return true;
+	return true;
 }
 my_string& my_string::operator += (const my_string & text){
-	int b=text.tam;
-	int i=0;
-	int c=0;
-	char * e=new char[b+tam];
-	for( i=0;i<tam;i++)e[i]=A[i];
-    for( i=tam-1;i<tam+b;i++)e[i]=text.A[c++];
-    delete[]A;
-    A=new char[b+tam];
-    for( i=0; i<tam+b; i++)A[i]=e[i];
-    delete[]e;
+	int b = text.siz;
+	int i = 0;
+	int c = 0;
+	char * e = new char[b+tam];
+	for( i = 0;i < siz; i++)e[i] = A[i];
+	for( i = siz - 1;i < siz + b ;i++)e[i] = text.A[c++];
+	delete[]A;
+	A = new char[b+siz];
+	for( i = 0; i < siz + b; i++) A[i] = e[i];
+	delete[]e;
 }
 my_string & my_string:: operator += (const  char* text){
-    int i=0;
-    int b=0;
+	int i = 0;
+	int b = 0;
 	while(text[i] != '\0'){
-            i++;
-            b++;}
-	int c=0;
-	char * e=new char[b+tam];
-	for( i=0;i<tam;i++)e[i]=A[i];
-    for( i=tam-1;i<tam+b;i++) e[i]=text[c++];
-    delete[]A;
-    A=new char[b+tam];
-    for( i=0; i<tam+b; i++)A[i]=e[i];
-    delete[]e;
+	    i++;
+	    b++;}
+	int c = 0;
+	char * e = new char[b + siz];
+	for( i = 0;i < siz; i++) e[i] = A[i];
+	for( i = siz-1 ; i < siz + b ; i++) e[i] = text[c++];
+	delete[]A;
+	A=new char[b + siz];
+	for( i=0; i < siz + b; i++)A[i] = e[i];
+	delete[]e;
 }
 int main(){
 prueba();
