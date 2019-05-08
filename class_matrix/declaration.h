6c0ee0cda6854c76afd3a2f7699c123e5d3e2f93
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-08 09:31:14 -0500

    Update declaration.h

--- class_matrix/declaration.h
+++ class_matrix/declaration.h
@@ -1,14 +1,20 @@
 #ifndef DECLARATION_H_INCLUDED
 #define DECLARATION_H_INCLUDED
 #include<iostream>
+#include <stdlib.h>
+#include<algorithm>
+#include<ctime>
+#include<cstdlib>
+#include<cstring>
+#include<assert.h>
 using namespace std;
 template<class E>
 class Matrix{
 
     private:
 
-    int row,col,R=0,C=0;
-    E** data;
+    int row , col , R , C;
+    E* data;
 
     public:
 
@@ -18,32 +24,32 @@ class Matrix{
 
     	Matrix(const Matrix<E> & summ );
 
-	~Matrix();
+		~Matrix();
 
-    	void llenar();
+    	void fill_in ();
 
     	Matrix<E> & operator = (const Matrix<E> & summ );
 
-	Matrix<E>   operator +(const Matrix<E> & summ );
+  		Matrix<E>   operator +(const Matrix<E> & summ );
 
-	Matrix<E>   operator -(const Matrix<E> & summ );
+  		Matrix<E>   operator -(const Matrix<E> & summ );
 
-	Matrix<E>   operator *(const Matrix<E> & summ);
+  		Matrix<E>   operator *(const Matrix<E> & summ);
 
-	Matrix<E> & operator *(double a);
+  		Matrix<E> & operator *(double a);
 
-	Matrix<E> & operator /(double a);
+  		Matrix<E> & operator /(double a);
 
-	Matrix<E> & operator << (E v);
+  		Matrix<E> & operator << (E v);
 
         E  & operator ()(int a, int b);
 
-  	friend std::ostream &operator << (std::ostream & os, const Matrix <E> & m)
+  		friend std::ostream &operator << (std::ostream & os, const Matrix <E> & m)
             {
-            for(int i = 0; i < m.row ;i++)
+            for(int i = 0; i < m.col ;i++)
                 {
-                for(int j = 0; j < m.col ;j++)
-                    os<<m.data[i][j]<<" ";
+                for(int j = 0; j < m.row ;j++)
+                    os<<m.data[j*m.row+i]<<" ";
                 std::cout<<std::endl;
                 }
                 return os;
@@ -54,8 +60,8 @@ class Matrix{
 template<class E>
 Matrix<E>:: Matrix()
     {
-    col = 0;
-    row = 0;
+    col = 0;row = 0;
+    R = 0 ; C = 0;
     data = new E[1];
     data[0] = new E[1];
     }
@@ -63,11 +69,9 @@ Matrix<E>:: Matrix()
 template<class E>
 Matrix<E>::Matrix(int r,int c)
     {
-    col = c;
-    row = r;
-    data = new E*[r];
-    for (int i = 0 ; i < row ; i++)
-        data[i] = new E[c];
+    col = c;row = r;
+    R = 0 ; C = 0;
+    data = new E [col*row];
     }
 
 template<class E>
@@ -75,103 +79,89 @@ Matrix<E>::Matrix(const Matrix<E> & summ )
     {
     col = summ.col;
     row = summ.row;
-    data = new E*[row];
-    for (int i = 0 ; i < row ; i++)
-        data[i] = new E[col];
-    for(int i = 0 ; i < row ; i++)
-        for (int j = 0 ; j < col ; j++)
-            data[i][j] = summ.data[i][j];
-    }
+    R = 0 ; C = 0;
+    data = new E [col*row];
+    memcpy(data ,summ.data,sizeof(E)*(col*row));}
 
 template<class E>
 Matrix<E>:: ~Matrix()
     {
-    for(int i = 0 ; i < row ; i++)
-        delete[] data[i];
     delete[] data;
     }
 
 template<class E>
-void Matrix<E>::llenar()
+void Matrix<E>::fill_in()
     {
-	for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-            	data[i][j] = 0;
+	for (int i = 0 ; i < col*row ; i++)
+            	data[i] = 0;
     }
 
 template<class E>
-Matrix<E>& Matrix<E>::  operator = (const Matrix<E> & summ )
-{
-    if(row == summ.row && col==summ.col)
-     for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-                data[i][j] = summ.data[i][j];
+Matrix<E>& Matrix<E>::  operator = (const Matrix<E> & summ ){
+    delete[] data;
+    col = summ.col;
+    row = summ.row;
+    R = 0 ; C = 0;
+    data = new E [col*row];
+    memcpy(data ,summ.data,sizeof(E)*(col*row));
     return *this;
 }
 
 template<class E>
 Matrix<E> Matrix<E>::  operator +(const Matrix<E> & summ )
     {
-    if(row == summ.row && col==summ.col)
-    {
-        Matrix <E> aux(row,col);
-        for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-              aux.data[i][j] = data[i][j] + summ.data[i][j];
-        return aux;
-        }
+    assert (row == summ.row && col==summ.col);
+    Matrix <E> aux(row,col);
+    for (int i = 0 ; i < row ; i++)
+        for (int j = 0 ; j < col ; j++)
+            aux.data[j*row+i] = data[j*row+i] + summ.data[j*row+i];
+    return aux;
     }
 
 template<class E>
 Matrix<E>  Matrix<E>::  operator -(const Matrix<E> & summ )
     {
 
-    if(row == summ.row && col==summ.col)
-    {
-        Matrix <E> aux(row,col);
-        for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-              aux.data[i][j] = data[i][j] - summ.data[i][j];
-        return aux;
-        }
+    assert(row == summ.row && col==summ.col);
+    Matrix <E> aux(row,col);
+    for (int i = 0 ; i < row ; i++)
+        for (int j = 0 ; j < col ; j++)
+            aux.data[j*row+i] = data[j*row+i] + summ.data[j*row+i];
+    return aux;
     }
 
 template<class E>
 Matrix<E> Matrix<E>::  operator *(const Matrix<E> & summ )
     {
-	 if(col == summ.row)
-        {
-        Matrix <E> aux(row,summ.col);
-        aux.llenar();
-        for (int i = 0 ; i < aux.row ; i++)
-            for (int j = 0 ; j < aux.col ; j++)
-             	for(int k = 0 ; k < col ; k++)
-              		aux.data[i][j] += data[i][k] * summ.data[k][j];
-        return aux;
-        }
+    assert(col == summ.row);
+    Matrix <E> aux(row,summ.col);
+    aux.fill_in();
+    for (int i = 0 ; i < aux.row ; i++)
+        for (int j = 0 ; j < aux.col ; j++)
+            for(int k = 0 ; k < col ; k++)
+                aux.data[j*row+i] +=data[k*row+i] * summ.data[j*row+k];
+    return aux;
     }
 
 template<class E>
 Matrix<E>& Matrix<E>::  operator *(double a)
     {
-    for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-                data[i][j] *= a;
+    for (int i = 0 ; i < row*col ; i++)
+                data[i]*= a;
     return *this;
     }
 
 template<class E>
 Matrix<E>& Matrix<E>::  operator /(double a){
-    for (int i = 0 ; i < row ; i++)
-            for (int j = 0 ; j < col ; j++)
-                data[i][j] /= a;
+    for (int i = 0 ; i < row *col; i++)
+                data[i]/= a;
     return *this;
 }
 
 template<class E>
 E & Matrix<E>::  operator ()(int a, int b)
     {
-    return data[a][b];
+    return data[b*row+a];
     }
 template<class E>
 Matrix<E> & Matrix<E>:: operator << (E v)
@@ -184,7 +174,7 @@ Matrix<E> & Matrix<E>:: operator << (E v)
         {
         C = 0;R=0;
         }
-    data[R][C++] = v;
+    data[(C++)*row+R] = v;
     return *this;
     }
 
