bc63c76c03953379f1a1368a3782021c7aa16f27
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-08 10:12:40 -0500

    Update 10341-Solve_It.cpp

--- UVA/10341-Solve_It.cpp
+++ UVA/10341-Solve_It.cpp
@@ -2,26 +2,25 @@
 #include <cstdio>
 #include <cmath>
 #include <iomanip>
-
 using namespace std;
 
 int main() {
 	double p, q, r, s, t,u, resultado,mn,mx,x;
-
 	bool sol;
 	while(cin>>p>>q>>r>>s>>t>>u){
-		mn=0.0;
-		mx=1.0;
-		sol=true;
-		while(mn!=mx){
-            x=(mn+mx)/2;
+		mn = 0.0;
+		mx = 1.0;
+		sol = true;
+		while(mn != mx){
+            		x = (mn + mx) / 2;
 			resultado=p * exp(-x) + q * sin(x) + r * cos(x) + s * tan(x) + t * pow(x,2) + u;
 			if (abs(resultado)<=0.0000001 ) {
 			cout << fixed << setprecision(4) << x << endl;
-			sol=false;
+			sol = false;
 			break;}
-			if(resultado>0.0)mn=x;
-            else mx=x;}
+			if(resultado > 0.0) mn = x;
+           	
+			else mx = x;}
 
 			if(sol) cout<<"No solution"<<endl;
 	}
