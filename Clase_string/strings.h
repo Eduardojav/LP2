f3a1669b7a9d886c4771e12a1dc98ff431f426a4
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-08 10:40:30 -0500

    Update strings.h

--- Clase_string/strings.h
+++ Clase_string/strings.h
@@ -8,15 +8,15 @@ class my_string{
 		int tamo(char * text=NULL);
 		int siz;
 	public:
-	    void print(){cout<<"este es el tamaño  "<<tam<<endl;}
+	    	void print(){cout<<"este es el tamaño  "<<tam<<endl;}
 		my_string();
 		my_string(const char * text);
 		my_string(const my_string & text);
 		~my_string();
 		my_string & operator = (const my_string & text);
 		my_string & operator = (const char* text);
-        my_string operator + (const my_string  text);
-        my_string operator + (const char* text);
+		my_string operator + (const my_string  text);
+		my_string operator + (const char* text);
 		my_string & operator += (const my_string & text);
 		my_string & operator += (const char* text);
 		my_string &operator[](const my_string & text);
@@ -27,13 +27,14 @@ class my_string{
 		bool  operator >= (const my_string & text);
 		char & operator[](int  i);
 		friend std::istream &operator >>(std::istream & is, my_string &str){
-            char* c = new char[1000];
-            is >> c;
-            str = my_string(c);
-            delete[] c;
-            return is;
-        }
-        friend std::ostream &operator <<(std::ostream & os,const my_string &str){
-            return os<<str.A;
-    }
+			    char* c = new char[1000];
+			    is >> c;
+			    str = my_string(c);
+			    delete[] c;
+			    return is;
+		}
+        	friend std::ostream &operator <<(std::ostream & os,const my_string &str){
+           	
+			return os<<str.A;
+   		 }
 };
