72203da3a27be3f76e4b95275cc35a084031a1d0
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-08 10:23:34 -0500

    Update merge_inversion.cpp

--- merge_forms/merge_inversion.cpp
+++ merge_forms/merge_inversion.cpp
@@ -7,7 +7,10 @@ void inversion (int *A,int i,int m, int j,int& z){
         while(b <= m){
             if(A[b] > A[d++])z++;
             if(d > j){b++;
-                d = m + 1;}}}
+                d = m + 1;
+                     }
+        }
+}
 void inversion (int *A,int i, int j,int& z ){
     if(i == j)return;
     int m = ( i + j ) / 2;
@@ -15,17 +18,14 @@ void inversion (int *A,int i, int j,int& z ){
     inversion(A,m+1,j,z);
     inversion(A,i,m,j,z);}
 void inversion(int *A,int & z,int j,int i=0){
-for(i;i<j;i++)
-    for(int k =i+1 ; k < j ; k++){
-        if(A[i] > A[k]){
-            z++;
-        }
-    }
-}
+    for( i; i < j ; i++)
+        for(int k =i+1 ; k < j ; k++)
+            if(A[i] > A[k])
+                z++;
 int main(){
     int As[] = {8,2,7,5,4,1};
     inversion(As,c,6);
     inversion(As,0,6,z);
-     cout<<c<<" este es c"<<endl;
+    cout<<c<<" este es c"<<endl;
     cout<<z <<" este es z"<<endl;
-return 0;}
+    return 0;}
