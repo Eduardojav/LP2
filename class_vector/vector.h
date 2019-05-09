59a9e780c0aa721e019b913ff9d8c8d7ebd96c49
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-09 04:36:47 -0500

    Create vector.h

---
+++ class_vector/vector.h
@@ -0,0 +1,37 @@
+#ifndef VECTOR_H_INCLUDED
+#define VECTOR_H_INCLUDED
+template<class E>
+class vector{
+    private:
+
+        E * data;
+
+        int size;
+
+    public:
+
+        vector();
+
+        vector(int a);
+
+        vector(const vector<E>& ex);
+
+        ~vector();
+
+        void push_back(const E a);
+
+		void pop_back();
+
+		void swap(vector<E> & ex);
+
+		void erase(int a);
+
+		void erase(int a, int b);
+
+		const int si_ze();
+
+		E& operator[](unsigned i);
+
+};
+#include "vector.inl"
+#endif // VECTOR_H_INCLUDED
