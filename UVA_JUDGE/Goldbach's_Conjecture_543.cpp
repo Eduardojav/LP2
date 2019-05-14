f5720fa8f8678ffd0833aaaa2fabc19f6e65c409
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-13 23:19:16 -0500

    Update Goldbach's_Conjecture_543.cpp

--- UVA_JUDGE/Goldbach's_Conjecture_543.cpp
+++ UVA_JUDGE/Goldbach's_Conjecture_543.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
-#include<windows.h>
 using namespace std;
 int a[1000000];
 int b[1000000];
-void fill_in(int a[100000]){
-	for(int i=0;i<100000;i++)
+void fill_in(int a[1000000]){
+	for(int i=0;i<1000000;i++)
 		a[i]=0;
 }
 void primos(int a[],int b[],size_t n=10000){
@@ -16,18 +15,17 @@ void primos(int a[],int b[],size_t n=10000){
 			for(size_t j=i*i;j<n;j+=2*i){
 				b[j]=1;
 				}
-		}		
+		}
 }}
 void search_summ(int b,int a[]){
 	int i=0;
 	int dif=0;
 	int j=0;
-	while(a[i++]<b);
+	while(a[i++]<b&&a[i]!=0);
 	i--;
-	cout<<a[i]<<endl;
-	while(j<=i){
+	while(j<=i+1){
 		if(b==a[i]+a[j]){
-			cout<<b<< " = "<< a[j] <<" + " <<a[i]<<endl;
+            cout<< b <<" = "<< a[j] <<  " + " << a[i]<<endl;
 			return;
 		}
 		else if (b>a[i]+a[j])
@@ -42,7 +40,7 @@ int main(){
 	int val1,val2,numero;
 	fill_in(b);
 	primos(a,b,1000000);
-	while(cin>>numero){
+	while(cin>>numero&& numero>=6 && numero<=1000000){
 	search_summ(numero,a);
 	}
 	return 0;
