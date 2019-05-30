924e45cc34d3822e96d79fcf7ffaca3812ee3e16
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-30 10:31:55 -0500

    Update figure.h

--- QT_Creator/Creador de figuras/figure.h
+++ QT_Creator/Creador de figuras/figure.h
@@ -8,14 +8,26 @@ public:
     figure();
 };
 class circle:public figure{
+private:
+    int radio;
+    int x;
+    int y;
 public:
     circle();
 };
 class square:public figure{
+private:
+    int side;
+    int x;
+    int y;
 public:
     square();
 };
 class triangle:public figure{
+private:
+    int x1,y1;
+    int x2,y2;
+    int x3, y3;
 public:
     triangle();
 };
