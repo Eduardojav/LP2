be0855754891d58294b7fdbdb566f06fb0f2e0c0
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-05-30 14:00:46 -0500

    Update figure.cpp

--- QT_Creator/Creador de figuras/figure.cpp
+++ QT_Creator/Creador de figuras/figure.cpp
@@ -6,26 +6,30 @@
 #include <QPen>
 #include "figure.h"
 #include "mainwindow.cpp"
-circle::circle()
+#include "mainwindow.ui"
+circle::circle(int a, int b, int c)
 {
-  radio = rand() % 100 + 1;
-  x=rand()%pixmap->width();
-  y=rand()%pixmap->height();
-  painter->drawEllipse({x, y}, radio, radio);
+  radio = c;
+  x=a;
+  y=b;
 }
-square::square()
-{
- side=rand() % 100 + 1;
- x=rand()%pixmap->width();
- y=rand()%pixmap->height();
- painter->drawRect(x, y,side,side);
+void circle::draw(QPainter * &painters){
+    painters->drawEllipse({x, y}, radio, radio);
 }
-Triangle::triangle()
-{
-  int x1=rand()%pixmap->width();y1=rand()%pixmap->height();
-  int x2=rand()%pixmap->width();y2=rand()%pixmap->height();
-  int x3=rand()%pixmap->width();y3=rand()%pixmap->height();
-  painter->drawLine(b, c, p, e);
-  painter->drawLine(p, e, f, g);
-  painter->drawLine(f, g, b, c);
+square::square(int a, int b, int c){
+    x=a; y=b;side=c;
+    }
+void square::draw(QPainter * &painters){
+
+     painters->drawRect(x, y,side,side);
+}
+triangle::triangle(int a, int b, int c, int d, int e, int f){
+    x1=a;y1=b;
+    x2=c;y2=d;
+    x3=e;y3=f;
+}
+void triangle::draw(QPainter * &painters){
+    painters->drawLine(x1,y1,x2,y2);
+    painters->drawLine(x2,y2,x3,y3);
+    painters->drawLine(x3,y3,x1,x2);
 }
