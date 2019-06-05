4d4ed2c9f32de3f3db0ced69f2a17e51d6c09673
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-06-05 05:33:34 -0500

    Update 10226_Hardwood-Species.cpp

--- UVA_JUDGE/10226_Hardwood-Species.cpp
+++ UVA_JUDGE/10226_Hardwood-Species.cpp
@@ -1,40 +1,45 @@
 #include <cstdio>
+#include <iostream>
+#include <iomanip>
 #include <string>
 #include <cstring>
+#include<string.h>
 #include <map>
 using namespace std;
 
 int main() {
-	int n;
-	map<string, int> logbook;
+	int cases;
+	map<string, int> trees;
+	map<string, int>::iterator it;
 	char name[35];
 
-	scanf("%d\n\n", &n);
-	while (n--) {
+	int a=cases;
+	scanf("%d\n\n",&cases);
+	while (cases--) {
 		int total = 0;
-
 		total = 0;
 		while (gets(name)) {
-			if (strlen(name) == 0)
-				break;
+			if (strlen(name) == 0){
+				break;}
 
-			if (logbook.count(string(name)) == 0) {
-				logbook[string(name)] = 1;
-			} else
-				logbook[string(name)] = logbook[string(name)] + 1;
+			if (trees.count(name) == 0)
+				trees[name] = 1;
+            else
+				trees[name] = trees[name] + 1;
 
 			total++;
 		}
+		for (it = trees.begin(); it != trees.end(); it++)
+            cout<<(*it).first.data()<< " "<<std::fixed << std::setprecision(4) <<(double) (*it).second/(double) total* 100.0<<endl;
+		if (cases > 0)
+        {
+			cout<<endl;
+			trees.clear();
+		}
 
-		map<string, int>::iterator it;
-		for (it = logbook.begin(); it != logbook.end(); it++) {
-			printf("%s %.4lf\n", (*it).first.data(),
-					(double) (*it).second / (double) total * 100.0);
-		}
-		if (n > 0){
-			printf("\n");
-			logbook.clear();
-		}
+	}
+	return 0;
+}
 	}
 
 	return 0;
