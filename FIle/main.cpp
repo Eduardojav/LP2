ae969d94340ef4d36c1e8271c954289120f756a2
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2019-06-19 12:50:56 -0500

    Update main.cpp

--- FIle/main.cpp
+++ FIle/main.cpp
@@ -9,43 +9,36 @@ char str[100];
 friend ostream & operator <<(ostream& os , A&a){
 	os<<a.x<<" "<<a.str;
 	return os;
-
 }
 friend istream & operator >>(istream & is, A& a){
 
 		is>>a.x>>a.str;
 	return is;
 }
-
 };
 void save_array(vector<A> data){
-	ofstream fs ("out.txt",ios::binary);
+	ofstream fs ("out.txt",ios::app|ios::binary);
 	for(int i=0;i<data.size();i++)
         	fs.write( (char *) &data[i], sizeof( A ) );
     fs.close();
 }
-void read(vector<A> &a){
+void read(){
 	A b;
 	ifstream fs("out.txt",ios::binary);
 		while( fs.read( (char *) &b, sizeof( A ) ) ) {
-		a.push_back(b);
 		cout << b << endl;
 		}
 		fs.close();
-		cout<<a.size()<<endl;
-
 
 }
-
 int main(){
 A a;
 vector <A> va;
-read(va);
+read();
 cout<<"hola"<<endl;
 	while(cin>>a){
 		va.push_back(a);}
-cout<<"salio"<<va[0]<<endl;
-cout<<va.size()<<endl;
+cout<<"salio"<<endl;
 save_array(va);
 return 0;
 }
